3739a48e4b1c51dad1db199e45b8e803725e1e22
Angel <angelnovo15@gmail.com>
2025-07-02 11:48:49 +0200

    Fixed serviceworker not always loading

--- public/js/serviceworker.js
+++ public/js/serviceworker.js
@@ -12,16 +12,17 @@ const createPWA = async () => {
 
 const RegisterWorker = () => {
     if(typeof window === 'undefined' ) return;
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker
-        .register(`/js/serviceworker.js`)
-        .then(_ => { /* Service worker created */})
-        .catch(err => { console.log("Service worker could not be created:", err); });
-    }
     window.addEventListener('beforeinstallprompt', e => {
         e.preventDefault();
         deferredPrompt = e;
         document.getElementById('ConfirmInstallPWA')?.addEventListener('click', _ => createPWA());
     });
     
+}
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register(`/js/serviceworker.js`)
+        .then(_ => { /* Service worker created */})
+        .catch(err => { console.log("Service worker could not be created:", err); });
 }
