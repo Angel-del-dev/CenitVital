06cd3334c33e1294dc5fa1dc813f89626c4dee02
Angel <angelnovo15@gmail.com>
2025-07-13 19:12:20 +0200

    Total bookings Data grid

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Http\Helpers\AdminMenu;
 use App\Http\Helpers\Calculus;
+use App\Models\Events;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 
@@ -43,12 +45,25 @@ class DashboardController extends Controller
         return $customers;
     }
 
+    private function GetEventsData() {
+        return Events::select([
+            DB::raw("DATE_FORMAT(date, '%Y-%m') as date"),
+            DB::raw('COUNT(*) as Total')
+        ])
+            ->whereYear('date', Carbon::now()->year)
+            ->groupBy(DB::raw("DATE_FORMAT(date, '%Y-%m')"))
+            ->orderBy(DB::raw("DATE_FORMAT(date, '%Y-%m')"), 'asc')
+            ->get()
+            ->toArray();
+    }
+
     public function create(string $lang) {
         if(Auth::user()->role->name !== 'ADMIN') return redirect("/{$lang}/my-place");
         return Inertia::render('admin/Dashboard', [
             'lang' => $lang, 
             'menu' => (new AdminMenu())->GetMenu($lang),
-            'customers' => $this->GetCustomers()
+            'customers' => $this->GetCustomers(),
+            'events' => $this->GetEventsData()
         ]);
     }
 }
--- resources/js/pages/admin/Dashboard.vue
+++ resources/js/pages/admin/Dashboard.vue
@@ -5,24 +5,30 @@ import DownloadApp from '@/components/generic/popup/DownloadApp.vue';
 import { useLanguage } from '@/composables/useLanguage';
 import { Head } from '@inertiajs/vue3';
 import { UsersRound } from 'lucide-vue-next';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 
-    const { lang, menu, customers } = defineProps(['lang', 'menu', 'customers']);
-    const loading = ref(true);
-    let translation = ref({});
-    const page_location = ref([]);
-    
-    onMounted(async () => {
-        translation.value = await useLanguage(lang, 'generic');
-        loading.value = false;
-        page_location.value.push({ Caption: translation.value.page_admin_dashboard, Link: `/${lang}/panel` });
-    });
+import { Line } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
+import DataGrid from '@/components/generic/visualize/DataGrid.vue';
+ChartJS.register(Title, Tooltip, LineElement, CategoryScale, LinearScale, PointElement);
 
-    const customers_percentage = ref(customers.percentage === null ? 'NaN' : customers.percentage);
-    let customers_percentage_color = ref('');
-    if(customers_percentage.value === 'NaN') customers_percentage_color.value = 'text-yellow';
-    else customers_percentage_color.value = customers_percentage.value < 0 ? 'text-red' : 'text-green';
-    
+const { lang, menu, customers, events } = defineProps(['lang', 'menu', 'customers', 'events']);
+const loading = ref(true);
+let translation = ref({});
+const page_location = ref([]);
+
+onMounted(async () => {
+    translation.value = await useLanguage(lang, 'generic');
+    loading.value = false;
+    page_location.value.push({ Caption: translation.value.page_admin_dashboard, Link: `/${lang}/panel` });
+});
+
+const customers_percentage = ref(customers.percentage === null ? 'NaN' : customers.percentage);
+let customers_percentage_color = ref('');
+if(customers_percentage.value === 'NaN') customers_percentage_color.value = 'text-yellow';
+else customers_percentage_color.value = customers_percentage.value < 0 ? 'text-red' : 'text-green';
+
+const eventColumns = ['date', 'chart_total_events'];
 
 </script>
 <template>
@@ -35,34 +41,48 @@ import { onMounted, ref } from 'vue';
         active="page_admin_dashboard"
     >
         <div
-            class="w-100 flex justify-start align-center gap-2 flex-wrap"
+            class="w-100 flex justify-start align-start direction-column gap-2"
         >
-            <Card
-                class="info-card"
+            <div
+                class="w-100 flex justify-start align-center gap-2 flex-wrap"
             >
-                <div
-                    class="flex justify-center align-start gap-3"
-                >   
-                    <div>
-                        <span class="flex justify-start align-start gap-1">
-                            <span class="text-gray">{{ translation.chart_new_customer }}</span>
-                            <span class="text-bold">{{ customers.current }}</span>
-                        </span>
-                        <span
-                            class="flex justify-center align-center gap-1"
-                            :title="customers.last"
-                        >
-                            <span :class="customers_percentage_color">
-                            {{ customers_percentage === 'NaN' ? customers_percentage : `${customers_percentage}%` }}
+                <Card
+                    class="info-card"
+                >
+                    <div
+                        class="flex justify-center align-start gap-3"
+                    >   
+                        <div>
+                            <span class="flex justify-start align-start gap-1">
+                                <span class="text-gray">{{ translation.chart_new_customer }}</span>
+                                <span class="text-bold">{{ customers.current }}</span>
                             </span>
-                            {{ translation.chart_since_last_month }}
-                        </span>
+                            <span
+                                class="flex justify-center align-center gap-1"
+                                :title="customers.last"
+                            >
+                                <span :class="customers_percentage_color">
+                                {{ customers_percentage === 'NaN' ? customers_percentage : `${customers_percentage}%` }}
+                                </span>
+                                {{ translation.chart_since_last_month }}
+                            </span>
+                        </div>
+                        <UsersRound :size="30" class="dashboard-icon" style="background-color: var(--blue);" />
                     </div>
-                    <UsersRound :size="30" class="dashboard-icon" style="background-color: var(--blue);" />
-                </div>
-            </Card>
+                </Card>
+            </div>
+            <div
+                class="w-100"
+            >
+                <DataGrid
+                    :translation="translation"
+                    :columns="eventColumns"
+                    :data="events"
+                    :onselect="() => null"
+                    :onnew="() => null"
+                />
+            </div>
         </div>
-        
     </Panel>
     <DownloadApp :translation="translation" />
 </template>
